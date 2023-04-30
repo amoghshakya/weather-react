2443df17c90384571b885870d7c32c86e67b77d3
Amogh Shakya <amoghshakya@outlook.com>
2023-04-30 22:58:46 +0545

    display weather data

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -59,53 +59,56 @@ export const Search = () => {
   const { data: cities } = fetchCity(searchTerm);
 
   return (
-    <div className="grid h-screen grid-cols-[10vw_1fr_10vw] grid-rows-[10vw_auto_1fr] place-items-center bg-[#00487C]">
-      <h1 className="col-start-2 row-start-1 m-4 text-4xl text-[#F8F1FF]">
-        Weather Search
-      </h1>
-      <div className="relative col-start-2 row-start-2 w-[calc(50%+4rem)] ">
-        <TbMapSearch className="absolute left-3 top-5" />
-        <input
-          onBlur={() => {
-            setTimeout(() => {
+    <>
+      <div className="grid h-auto grid-cols-[10vw_1fr_10vw] grid-rows-[10vw_auto_1fr] place-items-center">
+        <h1 className="col-start-2 row-start-1 m-4 text-4xl">
+          Weather Search
+        </h1>
+        <div className="relative col-start-2 row-start-2 w-[calc(50%+4rem)] ">
+          <TbMapSearch className="absolute left-3 top-5 stroke-black" />
+          <input
+            onBlur={() => {
+              setTimeout(() => {
+                if (inputRef.current) {
+                  inputRef.current.style.opacity = "0";
+                  setTimeout(() => {
+                    if (inputRef.current)
+                      inputRef.current.style.display = "none";
+                  }, 100);
+                }
+              }, 200);
+            }}
+            onFocus={() => {
               if (inputRef.current) {
-                inputRef.current.style.opacity = "0";
-                setTimeout(() => {
-                  if (inputRef.current) inputRef.current.style.display = "none";
-                }, 100);
+                inputRef.current.style.display = "block";
+                inputRef.current.style.opacity = "1";
               }
-            }, 200);
-          }}
-          onFocus={() => {
-            if (inputRef.current) {
-              inputRef.current.style.display = "block";
-              inputRef.current.style.opacity = "1";
-            }
-          }}
-          type="text"
-          placeholder="Search for a city..."
-          className="my-2 w-full rounded bg-[#4BB3FD] px-2 py-2 pl-9 shadow shadow-gray-800 outline-none transition placeholder:text-gray-500 hover:bg-[#3BA3ED] focus:bg-[#3BA3ED]"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <ul
-          ref={inputRef}
-          className="absolute w-full list-none self-start rounded bg-[#4BB3FD] shadow transition"
-        >
-          {cities?.map((city) => (
-            <li
-              onClick={() => setCityCoord(city.lat, city.lon)}
-              key={city.lat}
-              className="m-1 cursor-pointer rounded-sm px-4 py-2 transition hover:bg-[#027BCE]"
-            >
-              {city.name}, {city.state ? city.state + "," : ""} {city.country}
-            </li>
-          ))}
-        </ul>
+            }}
+            type="text"
+            placeholder="Search for a city..."
+            className="text-black hover:bg-[#3BA3ED] focus:bg-[#3BA3ED] my-2 w-full rounded bg-[#4BB3FD] px-2 py-2 pl-9 shadow shadow-gray-800 outline-none transition placeholder:text-gray-500"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <ul
+            ref={inputRef}
+            className="text-black absolute w-full list-none self-start rounded bg-[#4BB3FD] shadow transition"
+          >
+            {cities?.map((city) => (
+              <li
+                onClick={() => setCityCoord(city.lat, city.lon)}
+                key={city.lat}
+                className="hover:bg-[#027BCE] m-1 cursor-pointer rounded-sm px-4 py-2 transition"
+              >
+                {city.name}, {city.state ? city.state + "," : ""} {city.country}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <CityContext.Provider value={city}>
         <Weather />
       </CityContext.Provider>
-    </div>
+    </>
   );
 };
--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { APIKey } from "../apikey";
 import { CityContext, ICityCoord } from "./Search";
 import {
+  WiBarometer,
   WiCloudy,
   WiDayCloudy,
   WiDayRain,
@@ -11,6 +12,14 @@ import {
   WiDaySnow,
   WiDaySunny,
   WiDayThunderstorm,
+  WiDirectionDown,
+  WiDirectionDownLeft,
+  WiDirectionDownRight,
+  WiDirectionLeft,
+  WiDirectionRight,
+  WiDirectionUp,
+  WiDirectionUpLeft,
+  WiDirectionUpRight,
   WiNightAltRain,
   WiNightAltShowers,
   WiNightAltThunderstorm,
@@ -83,6 +92,18 @@ const weatherIcons: WeatherIcons = {
   _13d: <WiDaySnow className="icon-class" />,
   _13n: <WiNightSnow className="icon-class" />,
   _50d: <TbMist className="icon-class" />,
+  _50n: <TbMist className="icon-class" />,
+};
+
+const directionIcons: WeatherIcons = {
+  N: <WiDirectionDown className="direction-class" />,
+  NE: <WiDirectionDownLeft className="direction-class" />,
+  E: <WiDirectionLeft className="direction-class" />,
+  SE: <WiDirectionUpLeft className="direction-class" />,
+  S: <WiDirectionUp className="direction-class" />,
+  SW: <WiDirectionUpRight className="direction-class" />,
+  W: <WiDirectionRight className="direction-class" />,
+  NW: <WiDirectionDownRight className="direction-class" />,
 };
 
 const fetchWeather = (lat: number, lon: number) => {
@@ -90,6 +111,10 @@ const fetchWeather = (lat: number, lon: number) => {
     ["weather", lat, lon],
     async () => {
       const result = await axios.get<IWeatherResponse>(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`
+      );
+
+      console.log(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`
       );
 
@@ -112,38 +137,68 @@ export const Weather = () => {
     hours = hours % 12;
     hours = hours ? hours : 12;
     let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
 
-    return (
-      hours +
-      ":" +
-      (minutes < 10 ? "0" : "") +
-      minutes +
-      ":" +
-      (seconds < 10 ? "0" : "") +
-      seconds +
-      " " +
-      meridiem
-    );
+    return hours + ":" + (minutes < 10 ? "0" : "") + minutes + ":" + meridiem;
   }
 
   let icon = "_" + weather?.weather[0].icon;
 
+  function getDirection(degree: number) {
+    if (degree === 0) {
+      return "N";
+    } else if (degree > 0 && degree < 90) {
+      return "NE";
+    } else if (degree === 90) {
+      return "E";
+    } else if (degree > 90 && degree < 180) {
+      return "SE";
+    } else if (degree === 180) {
+      return "S";
+    } else if (degree > 180 && degree < 270) {
+      return "SW";
+    } else if (degree === 270) {
+      return "W";
+    } else if (degree > 270) {
+      return "NW";
+    } else {
+      return "";
+    }
+  }
+
   return (
-    <div className="col-start-2 row-start-3">
-      <h1>
-        {weather?.name} {weather?.sys.country}
-      </h1>
-      <p>{weather && "Temp: " + weather?.main.temp + "°C"}</p>
-      <p>{weather && "Feels like: " + weather?.main.feels_like + "°C"}</p>
-      <p>{weather && "Weather: " + weather.weather[0].description}</p>
-      <p>{weather && "Humidity: " + weather?.main.humidity}</p>
-      {weatherIcons[icon]}
-      <p>{weather && "Pressure: " + weather?.main.pressure + "hPa"}</p>
-      <p>{weather && "Min Temp: " + weather?.main.temp_min + "°C"}</p>
-      <p>{weather && "Max Temp: " + weather?.main.temp_max + "°C"}</p>
-      <p>{weather && "Sunrise: " + timeConverter(weather.sys.sunrise)}</p>
-      <p>{weather && "Sunset: " + timeConverter(weather.sys.sunset)}</p>
+    <div className="m-8 grid grid-cols-4 grid-rows-2 gap-4">
+      {weather && (
+        <>
+          <div className="col-start-1 row-start-1 flex flex-col gap-1 rounded-lg px-3 py-2">
+            <h1 className="text-2xl font-[800]">
+              {weather.name}, {weather.sys.country}
+            </h1>
+            <div className="flex">
+              <span>{weatherIcons[icon]}</span>
+              <span className="text-6xl">
+                {weather.main.temp.toPrecision(2) + "°C"}
+              </span>
+            </div>
+            <span className="flex">
+              {"Feels like " + weather.main.feels_like.toPrecision(2) + "°C"} ·{" "}
+              {weather.weather[0].description[0].toUpperCase() +
+                weather.weather[0].description.slice(1)}
+            </span>
+          </div>
+          <div className="row-start-2 flex gap-1 px-3 py-2">
+            <span className="flex">
+              {directionIcons[getDirection(weather.wind.deg)]}
+              {weather.wind.speed.toPrecision(2)}m/s{" "}
+              {getDirection(weather.wind.deg)}
+            </span>
+            ·
+            <span className="flex">
+              <WiBarometer className="h-10 w-10 my-[-.5rem]"/>
+              {weather.main.pressure} hPa
+            </span>
+          </div>
+        </>
+      )}
     </div>
   );
 };
